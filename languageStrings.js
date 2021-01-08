7fefc6044ed14b892df6addf420e57b01cae45a3
Jerry Qin <jerry.gn.qin@gmail.com>
2021-01-07 21:10:48 -0800

    updated voice ui/alexa speech

--- languageStrings.js
+++ languageStrings.js
@@ -1,20 +1,17 @@
 module.exports = {
   translation: {
-    SKILL_NAME: 'High Low Game',
+    SKILL_NAME: 'Fizz Buzz',
     EXIT_MESSAGE: 'Thanks for playing!',
-    FALLBACK_MESSAGE_DURING_GAME: `I can't help you with that.  Try guessing a number between 0 and 100. `,
-    FALLBACK_REPROMPT_DURING_GAME: 'Please guess a number between 0 and 100.',
-    FALLBACK_MESSAGE_OUTSIDE_GAME: `I can't help you with that.  I will come up with a number between 0 and 100 and you try to guess it by saying a number in that range. Would you like to play?`,
-    GUESS_CORRECT_MESSAGE: '%s is correct! Would you like to play again?',
-    LAUNCH_MESSAGE: 'Welcome to High Low guessing game. You have played %s times. Would you like to play?',
-    TOO_HIGH_MESSAGE: '%s is too high.',
-    TOO_HIGH_REPROMPT: 'Try guessing a smaller number.',
-    TOO_LOW_MESSAGE: '%s is too low.',
-    TOO_LOW_REPROMPT: 'Try guessing a larger number.',
+    FALLBACK_MESSAGE_DURING_GAME: `I dont\'t understand. Please say a number, fizz, buzz, or fizz buzz.`,
+    FALLBACK_MESSAGE_OUTSIDE_GAME: `Would you like to play Fizz Buzz? Please say help to get the rules of the game`,
+    NEXT_MESSAGE: '%s.',
+    NEXT_REPROMPT_MESSAGE: 'Please say a number, fizz, buzz, or both. Say help if you would like me to repeat the rules.',
+    LAUNCH_MESSAGE: 'Welcome to Fizz Buzz. We’ll each take turns counting up from one. However, you must replace numbers divisible by 3 with the word “fizz” and you must replace numbers divisible by 5 with the word “buzz”. If a number is divisible by both 3 and 5, you should instead say “fizz buzz”. If you get one wrong, you lose.',
     CONTINUE_MESSAGE: 'Say yes to play or no to quit.',
-    HELP_MESSAGE: 'I am thinking of a number between zero and one hundred, try to guess it and I will tell you if it is higher or lower.',
-    HELP_REPROMPT: 'Try saying a number.',
+    HELP_MESSAGE: 'We’ll each take turns counting up from one. However, you must replace numbers divisible by 3 with the word “fizz” and you must replace numbers divisible by 5 with the word “buzz”. If a number is divisible by both 3 and 5, you should instead say “fizz buzz”. If you get one wrong, you lose.',
+    HELP_REPROMPT: 'Try saying a number, fizz, buzz, or both',
     ERROR_MESSAGE: 'Sorry, an error occurred.',
-    YES_MESSAGE: 'Great! Try saying a number to start the game.'
+    LOST_MESSAGE: 'I\'m sorry, the correct answer was %s. You lose! Thanks for playing Fizz Buzz.',
+    YES_MESSAGE: 'Great! I will start... One.'
   }
 };
